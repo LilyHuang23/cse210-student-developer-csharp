aa6f620599b08af12cee802b42d86f16e5815f35
John Doe <l613426210@gmail.com>
2022-10-06 21:44:08 -0600

    hilo.game

---
+++ Unit02/card.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+
+namespace Unit02.Game
+{
+    // TODO: Implement the Die class as follows...
+    // 1) Add the class declaration. Use the following class comment.
+    public class Die{
+        public int value;
+        public int points;
+
+        public Die(){
+            value = 1;
+            points = 100;
+        }
+        public void Roll(){
+            Random r = new Random();
+            value = r.Next(1,7);
+            if (value == 1){
+                points = 100;
+            }
+            else if(value == 5){
+                points = 50;
+            }
+            else {
+                points = 0;
+            }
+        
+        }
+
+    }
+
+    }
+        /// <summary>
+        /// A small cube with a different number of spots on each of its six sides.
+        /// 
+        /// The responsibility of Die is to keep track of its currently rolled value and the points its
+        /// worth.
+        /// </summary> 
+
+
+    // 2) Create the class constructor. Use the following method comment.
+
+        /// <summary>
+        /// Constructs a new instance of Die.
+        /// </summary>
+
+    
+    // 3) Create the Roll() method. Use the following method comment.
+        
+        /// <summary>
+        /// Generates a new random value and calculates the points for the die. Fives are 
+        /// worth 50 points, ones are worth 100 points, everything else is worth 0 points.
+        /// </summary>
+        
